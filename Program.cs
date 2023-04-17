a51d6e1ec777df70c9248d9c9147d66f2d7763c6
Duane Scheepers <98160374+DuaneScheepersWebDeveloper@users.noreply.github.com>
2023-04-17 10:47:48 +0200

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hello_World
+{
+    internal class Program
+    {
+        //entry point of our program
+        static void Main(string[] args)
+        {
+ 
+            string myName = "Duane";
+            string capsMessage = myName.ToUpper();
+            string lowerMessage = capsMessage.ToLower();
+            Console.WriteLine("My name is "+ capsMessage);
+        }
+    }
+}
